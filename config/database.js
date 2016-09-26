11181a64f65f5917d4b243e60c72f3af7c7cdece
zak <zak.j.holt@gmail.com>
2016-09-26 16:20:03 -0400

    switch to ejs for templating

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url': 'mongodb://pup:pal@ds044989.mlab.com:44989/pup_pal'
+};
--- server.js
+++ server.js
@@ -1,8 +1,41 @@
 var express = require('express');
 var app = express();
-app.use(express.static('public'));
+var port = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+//require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'zakisgreat'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(express.static(__dirname + '/public'));
+
+require('./app/routes.js')(app, passport);
+
+
 app.listen(process.env.PORT || 8080, function() {
-    console.log("server is running at http://localhost:8080");
+    console.log("server is running at http://localhost:" + port);
 });
 
 exports.app = app;
